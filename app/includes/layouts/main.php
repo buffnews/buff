3368e997b1e06d7dd54fe9dd4e0a4a8ef2f29ec7
Frable <bfrable@ismac262.buffnews.com>
2015-01-06 15:58:44 -0500

    article page

---
+++ app/includes/layouts/main.php
@@ -0,0 +1,133 @@
+<div id="main">
+	<article>
+		<div class="article-title">
+			<h2>In Buffalo School Board debut, Ogilvie vows to shift focus to the classroom</h2>
+		</div>
+
+		<div class="article-image-main">
+			<img src="#" alt="">
+		</div>
+
+		<div class="article-image-main-caption">
+			<h3>This is the caption of the photo. There are a bunch of people, including C. Paladino, sitting around a table talking about education. (Photo: George Clooney)</h3>
+		</div>
+
+		<div class="article-data">
+			<ul>
+				<li class="author">
+					By <span class="name">Mark Gaughan</span>
+				</li>
+
+				<li class="contact">
+					<a href="#"><span class="email-icon"></span></a>
+					<a href="#"><span class="twitter-icon"></span></a>
+				</li>
+				
+				<li class="author">
+					By <span class="name">Mark Gaughan</span>
+				</li>
+
+				<li class="contact">
+					<a href="#"><span class="email-icon"></span></a>
+					<a href="#"><span class="twitter-icon"></span></a>
+				</li>
+
+				<li class="updated">
+					<p>updated 9:29 AM EST, Tuesday December 30, 2014</p>
+				</li>
+			</ul>
+		</div>
+
+		<div class="article-content-container">
+			<div class="socmed-util">
+				<ul>
+					<li class="mail">
+						
+					</li>
+					<li class="facebook">
+						
+					</li>
+					<li class="twitter">
+						
+					</li>
+					<li class="google">
+						
+					</li>
+					<li class="reddit">
+						
+					</li>
+					<li class="linkedin">
+						
+					</li>
+					<li class="more">
+						
+					</li>
+				</ul>
+			</div>
+
+			<div class="article-content">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet doloribus soluta esse consectetur, voluptatem, aspernatur aperiam quaerat itaque ut dicta veniam! Doloremque quibusdam dolorem earum eum. Unde, illo saepe quisquam.</p>
+				
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet porro excepturi adipisci optio saepe nam vero a, aliquam error voluptates quam molestiae quo recusandae, iusto ea provident unde, fuga eius.</p>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos aliquid enim corrupti necessitatibus assumenda mollitia maiores quasi, nobis tenetur. Voluptate quo, commodi est beatae voluptatem voluptatum eaque itaque expedita numquam.</p>
+
+				<div class="article-image">
+
+					<div class="article-image-container">
+						<img src="#" alt="">
+					</div>
+
+					<div class="article-image-caption">
+						<p>This is the caption of the photo. There are a bunch of people, including C. Paladino, sitting around a table talking about education. (Photo by George Clooney)</p>
+					</div>
+				</div>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex repudiandae quas dolores earum, eveniet optio vitae quos. Architecto dolores recusandae quidem ratione tempore, nemo velit exercitationem, eaque quas magnam voluptate!</p>
+				
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus velit esse, soluta est molestiae iure eum maxime, nihil iste veniam at impedit qui quos eaque deleniti, rerum adipisci tempora non.</p>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, quae corrupti ullam odit. Eos repudiandae vel temporibus quisquam fugit sunt, quas alias. Doloremque beatae illo, quas officia nisi voluptate consequuntur!</p>
+				
+				<div class="content-with-related-story-widget">
+					<div class="content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit omnis minus nam natus sequi optio ea, fugiat veritatis, assumenda accusantium ab magni, mollitia voluptatem quisquam nobis beatae excepturi! Optio, iste?</p>
+						
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium explicabo illo quasi, quisquam, aliquid libero impedit cumque? Ipsum, eum, quibusdam, ut pariatur doloribus labore fugit, culpa aliquid illum nesciunt laboriosam.</p>
+
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur veritatis officia aut et at distinctio, dolor temporibus minus laudantium velit, est incidunt vitae vero. Blanditiis sit labore ad dolore.</p>
+					
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae quod omnis, quia saepe nemo dolores nihil velit error minima inventore sed, unde, quas perspiciatis ducimus odit expedita voluptate veniam et.</p>
+					</div>
+					
+					<div class="related-stories-widget">
+						<div class="wrapper">
+							<h3>More related to this story</h3>
+							
+
+							<ul class="related-stories">
+								<li class="main">
+									<div class="img-container">
+										<img src="#" alt="">
+									</div>
+									
+									<h4>New Board looks to Ogilive to reset  district's course</h4>	
+								</li>
+
+								<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil molestias labore, neque quidem et sint nesciunt repudiandae blanditiis veritatis adipisci. Ratione eligendi quidem aliquam expedita reiciendis delectus suscipit, non, autem?</li>
+
+								<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem similique consequatur nulla quaerat error ducimus earum atque cum possimus laborum, dolor aspernatur ad ea. Ea veritatis, cumque assumenda corrupti suscipit!</li>
+
+								<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dolore minima quaerat alias debitis repudiandae similique, minus ullam accusamus, quod consectetur itaque pariatur, amet magni optio soluta sunt veritatis nobis!</li>
+							</ul>
+
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+
+	</article>
+</div>
--- dist/includes/base/head.php
+++
@@ -1,9 +0,0 @@
-<?php /* HEADER */ ?>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<title></title>
-<meta name="description" content="">
-<meta name="viewport" content="width=device-width">
-<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-<script src="scripts/vendor/b4f9a6ca.modernizr.js"></script>
-<link rel="stylesheet" href="styles/1c89e6d5.main.css">
