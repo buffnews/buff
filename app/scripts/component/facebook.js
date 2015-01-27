4c61d5bfcb40bb6180725d3021be413419778fb5
Frable <bfrable@Buffalos-MBP-2.home>
2015-01-26 20:46:16 -0500

    fix script

--- app/scripts/component/facebook.js
+++ app/scripts/component/facebook.js
@@ -1,16 +1,19 @@
-define(['require'],function (require) {
+define([],function () {
     'use strict';
 
     return {
         init: function(){
-
+			/* jshint ignore:start */
+			
 			(function(d, s, id) {
 			  var js, fjs = d.getElementsByTagName(s)[0];
 			  if (d.getElementById(id)) return;
 			  js = d.createElement(s); js.id = id;
-			  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=319615844848302&version=v2.0";
+			  js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=319615844848302&version=v2.0';
 			  fjs.parentNode.insertBefore(js, fjs);
 			}(document, 'script', 'facebook-jssdk'));
+
+			/* jshint ignore:end */
 
         }
     };
